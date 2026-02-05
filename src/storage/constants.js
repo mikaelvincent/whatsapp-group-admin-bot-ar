5f085d8c4e3a596623e5877e852cee286d4333a7
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2026-02-06 04:20:14 +0800

    Add storage constants module
    * Centralize store version and welcome defaults
    * Avoid duplicating magic numbers across modules

---
+++ src/storage/constants.js
@@ -0,0 +1,7 @@
+const STORE_VERSION = 5;
+
+const DEFAULT_WELCOME_TEMPLATE = '👋 مرحبًا {user}!\n\nأهلًا بك في المجموعة.\n\n{rules}';
+
+const MAX_WELCOME_TEMPLATE_CHARS = 2000;
+
+export { DEFAULT_WELCOME_TEMPLATE, MAX_WELCOME_TEMPLATE_CHARS, STORE_VERSION };
